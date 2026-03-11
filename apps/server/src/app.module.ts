aa4646461ddaef51463b0d4deae9c0d7cc823c8b
Darshan-P2331 <darshanp2331@gmail.com>
2026-03-11 19:23:42 +0530

    Added cookie-parser

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { JwtModule } from '@nestjs/jwt';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL!),
+    JwtModule.register({
+      global: true,
+      secret: process.env.ACCESS_TOKEN_SECRET!,
+      signOptions: { expiresIn: '60s' },
+    }),
+    UsersModule,
+    CategoriesModule,
+    ProductsModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('Resto')
+    .setDescription('The resto API description')
+    .setVersion('1.0')
+    .addTag('resto')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
